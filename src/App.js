a77e22699c358dbf1ffc392ab096d0a84877fbbc
Nisarg Wanve <nisargwanve1@gmail.com>
2026-03-07 07:54:30 -0800

    clinic dashboard

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import PatientForm from "./components/PatientForm";
+import TestForm from "./components/TestForm";
+import TestTable from "./components/TestTable";
 
 function App() {
+   const [search,setSearch] = useState("")
+   const filteredTests = tests.filter((t)=>
+t.patientName.toLowerCase().includes(search.toLowerCase())
+)
+  const [patients, setPatients] = useState([]);
+  const [tests, setTests] = useState([]);
+
+  const addPatient = (patient) => {
+    setPatients([...patients, patient]);
+  };
+
+  const addTest = (test) => {
+    setTests([...tests, test]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+
+      <h2 className="text-center mb-4">Clinic Dashboard</h2>
+
+      <PatientForm addPatient={addPatient} />
+
+      <TestForm patients={patients} addTest={addTest} />
+
+      <TestTable tests={tests} />
+
     </div>
   );
 }
